0824111f303b0f5e80e9530da5c1a21a0735a2db
Frank Fernandes <frankfernandes@gmail.com>
2014-01-20 14:33:57 -0800

    Initial Commit

---
+++ StowAway/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StowAway
+//
+//  Created by Francis Fernandes on 1/20/14.
+//  Copyright (c) 2014 Francis Fernandes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ StowAway/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  StowAway
+//
+//  Created by Francis Fernandes on 1/20/14.
+//  Copyright (c) 2014 Francis Fernandes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
