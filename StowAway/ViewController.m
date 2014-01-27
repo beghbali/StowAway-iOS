7e5d5f64777dd19e1176ad486ced47c16bf0b110
Frank Fernandes <frankfernandes@gmail.com>
2014-01-27 01:10:17 -0800

    When the User logs in a user is created/or updated

--- StowAway/ViewController.m
+++ StowAway/ViewController.m
@@ -44,17 +44,19 @@
                             user:(id<FBGraphUser>)user {
     self.profilePictureView.profileID = user.id;
     self.nameLabel.text = user.name;
-
+    
+    /*Send User Data to API on the user TODO Should be made into an object!!!!*/
     NSString *url =@"http://ec2-54-214-145-124.us-west-2.compute.amazonaws.com/api/v1/users/";
     
     // Like the post request I showed you before, I'm going to send the deviceId again, because that is generally useful for establishing
     
-    //UIDevice *device = [UIDevice currentDevice];
-    NSString *id = user.id;
-    NSString *first_name = user.first_name;
-    NSString *last_name = user.last_name;
+    NSDate *fbAccessTokenExpirationDate = [[[FBSession activeSession] accessTokenData] expirationDate];
+    NSString *fbAccessToken = [[[FBSession activeSession] accessTokenData] accessToken];
+    NSString *provider = @"facebook";
     
-    NSString *post = [NSString stringWithFormat:@"uid=%@,provider=facebook", id];
+    NSString *userdata = [NSString stringWithFormat:@"{\"first_name\":\"%@\", \"last_name\":\"%@\", \"image_url\":\"http://graph.facebook.com/%@/picture?type=square\", \"location\":\"%@\", \"profile_url\":\"https://www.facebook.com/%@\",\"token\":\"%@\",\"expires_at\":\"%@\"}",user.first_name,user.last_name,user.id,user.location.name,user.username,fbAccessToken,fbAccessTokenExpirationDate];
+    
+    NSString *post = [NSString stringWithFormat:@"{\"uid\":%@,\"provider\":\"%@\",\"user\":%@}", user.id, provider, userdata];
     NSData *postData = [post dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
     NSString *postLength = [NSString stringWithFormat:@"%d", [postData length]];
     
@@ -62,9 +64,7 @@
     [request setURL:[NSURL URLWithString:url]];
     [request setHTTPMethod:@"POST"];
     [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
-    //[request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
-    [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
-    
+    [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
     [request setHTTPBody:postData];
     
     NSURLResponse *response;
@@ -75,24 +75,13 @@
     NSDictionary *results = jsonData ? [NSJSONSerialization JSONObjectWithData:jsonData options:NSJSONReadingMutableContainers|NSJSONReadingMutableLeaves error:&error] : nil;
     
     if (error) NSLog(@"[%@ %@] JSON error: %@", NSStringFromClass([self class]), NSStringFromSelector(_cmd), error.localizedDescription);
-
+    /*End of Code for Adding User*/
     
-    /*
-    //Send Data to Endpoint to create user
-    NSURL *url = [NSURL URLWithString: [NSString stringWithFormat:@"http://ec2-54-214-145-124.us-west-2.compute.amazonaws.com/api/v1/users/"]];
-
-    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
-    
-    [request setURL:url];
-    [request setHTTPMethod:@"POST"];
-    [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
-    
-    NSError *error;
-    NSURLResponse *response;
-    NSData *data = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
-    
- 
-curl -X POST -d '{ "provider":"facebook", "uid":123123, "user":{"first_name":"Joe", "last_name":"Bloggs", "image_url":"http://graph.facebook.com/12345678/picture?type=square", "token":"AF4234B3C", "expires_at":1321747205, "gender":"male", "location":"Palo Alto, California", "verified":true, "profile_url":"https://www.facebook.com/thebashir"}}' http://ec2-54-214-145-124.us-west-2.compute.amazonaws.com//api/v1/users -H "Content-Type:application/json"
+    /*  Example for JSON data
+curl -X POST -d '{ "provider":"facebook", 
+     "uid":123123, 
+     "user":{"first_name":"Joe", "last_name":"Bloggs", "image_url":"http://graph.facebook.com/12345678/picture?type=square",
+     "token":"AF4234B3C", "expires_at":1321747205, "gender":"male", "location":"Palo Alto, California", "verified":true, "profile_url":"https://www.facebook.com/thebashir"}}' http://ec2-54-214-145-124.us-west-2.compute.amazonaws.com//api/v1/users -H "Content-Type:application/json"
      */
     
     
