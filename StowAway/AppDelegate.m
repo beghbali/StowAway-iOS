a402474369dab2f78d336c1950063e361cd4d8c4
Bashir Eghbali <bashir@gmail.com>
2014-04-23 00:56:03 -0700

    changed StowAway to Stowaway and ‘ok’ to ‘update’ and the version number

--- StowAway/AppDelegate.m
+++ StowAway/AppDelegate.m
@@ -283,7 +283,7 @@
                                                         message:@"You must update to the latest version of the app"
                                                        delegate:self
                                               cancelButtonTitle:nil
-                                              otherButtonTitles:@"OK", nil];
+                                              otherButtonTitles:@"Update", nil];
         [alert show];
     }
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
